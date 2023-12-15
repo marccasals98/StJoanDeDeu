d5fb6be413b46f0260ccf28932e20097bdd370e8
Marc Casals Salvador <97680577+marccasals98@users.noreply.github.com>
2023-12-15 11:23:22 +0100

    Added the ground truth, calculated the DER with different collars. (#10)
    * added the parlament ground truth
    
    * Calculated DER as Abir did it and tried different DERs

--- diarization/comparing_models/abir_method.py
+++ diarization/comparing_models/abir_method.py
@@ -2,14 +2,26 @@ import json
 from pyannote.core import Annotation, Segment
 from pyannote.metrics.diarization import DiarizationErrorRate
 from config import PATHS
+from label_studio import label_studio_info
+from pyannote_conversion import pyannote_info
+from whisperX_conversion import whisperX_info
 
+# Create the references:
 ref = Annotation()
 hyp = Annotation()
 
-ref_path = PATHS["ground_truth_simplified"]
-hyp_path = PATHS["pyannote_simplified"]
+# Import the references as list.
+ref_list = label_studio_info()
+hyp_list = pyannote_info()
 
+for reference in ref_list:
+    ref[Segment(reference[1], reference[2])] = reference[0]
 
+for hyphotesis in hyp_list:
+    hyp[Segment(hyphotesis[1], hyphotesis[2])] = hyphotesis[0]
+
+"""
+# This is the original Abir's version ---------------------------------------
 with open(ref_path, 'r', encoding='utf-8') as file:
         ground_truth = json.load(file)
 for item in ground_truth:
@@ -19,6 +31,8 @@ with open(hyp_path, 'r', encoding='utf-8') as file:
         preds = json.load(file)
 for item in preds:
         hyp[Segment(item['start'], item['end'])] = item['speaker']
+# ---------------------------------------------------------------------------
+"""
 
-diarizationErrorRate = DiarizationErrorRate(skip_overlap=True)
+diarizationErrorRate = DiarizationErrorRate(skip_overlap=False, collar=0.25)
 print("DER: ",diarizationErrorRate(ref, hyp, detailed=True))
--- diarization/comparing_models/config.py
+++ diarization/comparing_models/config.py
@@ -1,5 +1,7 @@
 PATHS = {
+    'ground_truth' : "/home/marc/Desktop/StJoanDeDeu/diarization/GROUND_TRUTH/project-1-at-2023-12-11-11-35-4f0adfaa-MIN.json",
     'ground_truth_simplified' : "diarization/GROUND_TRUTH/project-1-at-2023-12-11-11-35-4f0adfaa-MIN_simplified.json",
+    'pyannote' : "/home/marc/Desktop/StJoanDeDeu/diarization/PyAnnote/results.txt",
     'pyannote_simplified' : "diarization/PyAnnote/results_simplified.txt",
     'whisperX' : "diarization/WhisperX/whisperX.txt"
 }
