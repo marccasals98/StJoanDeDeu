d2cd009c83fba673ae31b573010fbb8dcaecbb3a
Marc Casals Salvador <97680577+marccasals98@users.noreply.github.com>
2023-12-19 11:33:03 +0100

    Added ProgressHook and Argparse with some documentation (#11)

--- diarization/PyAnnote/diarization_test.py
+++ diarization/PyAnnote/diarization_test.py
@@ -1,22 +1,70 @@
-from pyannote.audio import Pipeline
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.0",
-    use_auth_token="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")
+"""
+Speaker Diarization.
 
-# The path where the audio file is saved. 
-audio_path = "diarization/jueves_a_las_13-27.wav"
+This Script runs speaker diarization from the library Pyannote. 
 
-# send pipeline to GPU (when available)
+\b
+Requirements:
+1. pyannote.audio version 3.1 or higher.
+2. Hugging face account with huggingface token and the permission to use this script.
+
+"""
+
 import torch
-pipeline.to(torch.device("cpu"))
+from pyannote.audio import Pipeline
+from pyannote.audio.pipelines.utils.hook import ProgressHook
+import torchaudio
+import sys
+import argparse
 
-# apply pretrained pipeline
-diarization = pipeline(audio_path)
+def init_pipepline():
+    """
+    Initialize the pipeline.
+    """
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization-3.0",
+        use_auth_token="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")
 
-# print the result
-for turn, _, speaker in diarization.itertracks(yield_label=True):
-    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-# start=0.2s stop=1.5s speaker_0
-# start=1.8s stop=3.9s speaker_1
-# start=4.2s stop=5.7s speaker_0
-# ...
+    # send pipeline to GPU (when available)
+    pipeline.to(torch.device("cpu"))
+
+    print(f"tipus pipeline {type(pipeline)}")
+
+    return pipeline
+
+def run_diarization(pipeline, waveform, sample_rate):
+    """ 
+    Runs the diarization
+    """
+    # Use ProgressHook to track the progress of the pipeline.
+    with ProgressHook() as hook:
+            diarization, embeddings = pipeline({"waveform": waveform, "sample_rate": sample_rate}, 
+                                            hook=hook, return_embeddings=True)
+    return diarization, embeddings
+
+def print_results(diarization, embeddings):
+    """
+    Prints the diarization results ina  displayed form, taking into account the different turns.
+    """
+    for turn, _, speaker in diarization.itertracks(yield_label=True):
+        print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+
+def main():
+    pipleline = init_pipepline()
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        'audio_path',
+        type=str,
+        help="The path of the audio you want to diarize."
+    )
+    args = parser.parse_args()
+
+    waveform, sample_rate = torchaudio.load(args.audio_path)
+    
+    diarization, embeddings = run_diarization(pipleline, waveform, sample_rate)
+
+    print_results(diarization, embeddings)
+
+if __name__=='__main__':
+    sys.exit(main())
